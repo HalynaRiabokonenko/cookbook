829bdcba0da177b8c85d58c358746c058c32f5e3
Halyna Riabokonenko <sgallina89@gmail.com>
2023-10-19 10:39:02 +0200

    add recipes.json, component RecipeList.js and use it at Main.js

---
+++ src/RecipeList.js
@@ -0,0 +1,31 @@
+import React from "react";
+import recipesData from "./recipes.json";
+
+const RecipeList = () => {
+  return (
+    <div>
+      <h1>Recipes</h1>
+      <ul>
+        {recipesData.recipes.map((recipe) => (
+          <li key={recipe.id}>
+            <h2>{recipe.name}</h2>
+            <h3>ingredients:</h3>
+            <ul>
+              {recipe.ingredients.map((ingredient, index) => (
+                <li key={index}>{ingredient}</li>
+              ))}
+            </ul>
+            <h3>instructions:</h3>
+            <ol>
+              {recipe.instructions.map((instruction, index) => (
+                <li key={index}>{instruction}</li>
+              ))}
+            </ol>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RecipeList;

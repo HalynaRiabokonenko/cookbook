80c7dc355f3362497da047989f71dbda961ab9d7
Halyna Riabokonenko <sgallina89@gmail.com>
2024-02-16 21:41:56 +0100

    add classes to Home.tsx, remove Home.css

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,5 @@
-import React, { ReactElement, useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import RecipesDataInterface from "./Home.types";
-import "./Home.css";
 import { Link } from "react-router-dom";
 import styles from "./Home.module.css";
 import { ModeContext } from "../../providers/mode";
@@ -19,11 +18,11 @@ function Home() {
 
 
   return recipesData && (
-    <div className="home">
-      <main className="home-content">
-        <section className="home-content__container">
-          <h1 className="home-content__header">Delicious Ukrainian Cuisine</h1>
-          <p className="home-content__paragraph">
+    <div className={styles["home"]}>
+      <main className={styles["home-content"]}>
+        <section className={styles["home-content__container"]}>
+          <h1 className={styles["home-content__header"]}>Delicious Ukrainian Cuisine</h1>
+          <p className={styles["home-content__paragraph"]}>
             Welcome to our website, where we have gathered a treasure trove of
             the most renowned recipes from Ukrainian cuisine. Here, you will
             embark on a culinary journey through the heart of Ukraine, exploring
@@ -39,55 +38,55 @@ function Home() {
             and discover the magic of Ukrainian cuisine right here on our
             website.
           </p>
-          <div className="container-home__list--popular">
+          <div className={styles["container-home__list--popular"]}>
             <div>
-              <ul className="home__list--popular">
+              <ul className={styles["home__list--popular"]}>
                 {recipesData.recipes
                   .filter((recipe) => [1, 3, 6].includes(recipe.id))
                   .map((recipe) => (
                     <li
                       key={recipe.id}
-                      className="home-content__recipes-list--option"
+                      className={styles["home-content__recipes-list--option"]}
                     >
-                      <h2 className="home-content__recipes-name">
+                      <h2 className={styles["home-content__recipes-name"]}>
                         {recipe.name}
                       </h2>
-                      <div className="home-content__recipes-info">
-                        <p className="home-content__recipes-description">
+                      <div className={styles["home-content__recipes-info"]}>
+                        <p className={styles["home-content__recipes-description"]}>
                           {recipe.description}
                         </p>
-                        <div className="home-content__recipes-content">
-                          <div className="home-content__recipes-photo-container">
+                        <div className={styles["home-content__recipes-content"]}>
+                          <div className={styles["home-content__recipes-photo-container"]}>
                             <img
                               src={recipe.photoPath}
                               alt={recipe.name}
-                              className="home-content__recipes-photo"
+                              className={styles["home-content__recipes-photo"]}
                             ></img>
                           </div>
-                          <div className="home-content__recipes-ingredients-container">
-                            <h3 className="home-content__recipes-ingredients">
+                          <div className={styles["home-content__recipes-ingredients-container"]}>
+                            <h3 className={styles["home-content__recipes-ingredients"]}>
                               ingredients:
                             </h3>
-                            <ul className="home-content__ingredients-list">
+                            <ul className={styles["home-content__ingredients-list"]}>
                               {recipe.ingredients.map((ingredient, index) => (
                                 <li
                                   key={index}
-                                  className="home-content__ingredients-list--option"
+                                  className={styles["home-content__ingredients-list--option"]}
                                 >
                                   {ingredient}
                                 </li>
                               ))}
                             </ul>
                           </div>
-                          <div className="home-content__recipes-instructions-container">
-                            <h3 className="home-content__recipes-instructions">
+                          <div className={styles["home-content__recipes-instructions-container"]}>
+                            <h3 className={styles["home-content__recipes-instructions"]}>
                               instructions:
                             </h3>
-                            <ol className="home-content__instructions-list">
+                            <ol className={styles["home-content__instructions-list"]}>
                               {recipe.instructions.map((instruction, index) => (
                                 <li
                                   key={index}
-                                  className="home-content__instructions-list--option"
+                                  className={styles["home-content__instructions-list--option"]}
                                 >
                                   {instruction}
                                 </li>
@@ -101,10 +100,10 @@ function Home() {
               </ul>
             </div>
           </div>
-          <div className="home__all-recipes-container">
-            <p className="home__all-recipes-paragraph">
+          <div className={styles["home__all-recipes-container"]}>
+            <p className={styles["home__all-recipes-paragraph"]}>
               See all recipes{" "}
-              <Link to="/recipes" className="home__all-recipes-link">
+              <Link to="/recipes" className={styles["home__all-recipes-link"]}>
                 here
               </Link>
             </p>

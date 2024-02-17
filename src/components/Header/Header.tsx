37f46feea936f1a52eaac81f7895c99ade23e9a8
Halyna Riabokonenko <sgallina89@gmail.com>
2024-02-17 21:27:49 +0100

    add Header.module.css

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,50 +1,63 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Header.css";
+import styles from "./Header.module.css";
+import classnames from "classnames";
 
 function Header() {
   return (
-    <header className="header">
-      <div className="header__container">
-        <div className="logo">
-          <Link className="logo__link" to="/">
+    <header className={styles["header"]}>
+      <div className={styles["header__container"]}>
+        <div className={styles["logo"]}>
+          <Link className={styles["logo__link"]} to="/">
             <img
               alt="Logo proven recipes"
               src="/logo.png"
-              className="logo__img"
+              className={styles["logo__img"]}
             />
           </Link>
         </div>
-        <div className="global-nav__container">
-          <nav className="global-nav">
-            <ul className="global-nav__list">
-              <li className="global-nav__list-item">
+        <div className={styles["global-nav__container"]}>
+          <nav className={styles["global-nav"]}>
+            <ul className={styles["global-nav__list"]}>
+              <li className={styles["global-nav__list-item"]}>
                 <Link
-                  className="global-nav__list-item-link global-nav__list-item-link--home"
+                              className={classnames(
+                                styles["global-nav__list-item-link"],
+                                styles["global-nav__list-item-link--home"]
+                              )}
                   to="/"
                 >
                   Home
                 </Link>
               </li>
-              <li className="global-nav__list-item">
+              <li className={styles["global-nav__list-item"]}>
                 <Link
-                  className="global-nav__list-item-link global-nav__list-item-link--contact"
+                 className={classnames(
+                    styles["global-nav__list-item-link"],
+                    styles["global-nav__list-item-link--contact"]
+                  )}
                   to="/recipes"
                 >
                   Recipes
                 </Link>
               </li>
-              <li className="global-nav__list-item">
+              <li className={styles["global-nav__list-item"]}>
                 <Link
-                  className="global-nav__list-item-link global-nav__list-item-link--about"
+                 className={classnames(
+                    styles["global-nav__list-item-link"],
+                    styles["global-nav__list-item-link--about"]
+                  )}
                   to="/about"
                 >
                   About us
                 </Link>
               </li>
-              <li className="global-nav__list-item">
+              <li className={styles["global-nav__list-item"]}>
                 <Link
-                  className="global-nav__list-item-link global-nav__list-item-link--contact"
+                className={classnames(
+                    styles["global-nav__list-item-link"],
+                    styles["global-nav__list-item-link--contact"]
+                  )}
                   to="/contact"
                 >
                   Contact us

633dc51b653dbf40dad7be2b37155000b92e2fc6
Halyna Riabokonenko <sgallina89@gmail.com>
2024-02-17 21:53:47 +0100

    add RecipeDetails.module.css

--- src/components/Recipes/RecipeDetails/RecipeDetails.tsx
+++ src/components/Recipes/RecipeDetails/RecipeDetails.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import "./RecipeDetails.css";
+import styles from "./RecipeDetails.module.css";
 import RecipesDataInterface from "./RecipeDetails.types";
 
 function RecipeDetails() {
@@ -28,44 +28,44 @@ function RecipeDetails() {
   }
 
   return recipesData && (
-    <main className="recipe-details-content">
-      <section className="recipe-details-content__container">
-        <h1 className="recipe-details-content__header">{recipe.name}</h1>
-        <p className="recipe-details-content__description">
+    <main className={styles["recipe-details-content"]}>
+      <section className={styles["recipe-details-content__container"]}>
+        <h1 className={styles["recipe-details-content__header"]}>{recipe.name}</h1>
+        <p className={styles["recipe-details-content__description"]}>
           {recipe.description}
         </p>
-        <div className="recipe-details-content__container-recipe-details">
-          <div className="recipe-details-content__photo-container">
+        <div className={styles["recipe-details-content__container-recipe-details"]}>
+          <div className={styles["recipe-details-content__photo-container"]}>
             <img
               src={recipe.photoPath}
               alt={recipe.name}
-              className="recipe-details-content__photo"
+              className={styles["recipe-details-content__photo"]}
             />
           </div>
-          <div className="recipe-details-content__ingredients-container">
-            <h3 className="recipe-details-content__recipes-ingredients">
+          <div className={styles["recipe-details-content__ingredients-container"]}>
+            <h3 className={styles["recipe-details-content__recipes-ingredients"]}>
               ingredients:
             </h3>
-            <ul className="recipe-details-content__ingredients-list">
+            <ul className={styles["recipe-details-content__ingredients-list"]}>
               {recipe.ingredients.map((ingredient, index) => (
                 <li
                   key={index}
-                  className="recipe-details-content__ingredients-list--option"
+                  className={styles["recipe-details-content__ingredients-list--option"]}
                 >
                   {ingredient}
                 </li>
               ))}
             </ul>
           </div>
-          <div className="recipe-details-content__instructions-container">
-            <h3 className="recipe-details-content__recipes-instructions">
+          <div className={styles["recipe-details-content__instructions-container"]}>
+            <h3 className={styles["recipe-details-content__recipes-instructions"]}>
               instructions:
             </h3>
-            <ol className="recipe-details-content__instructions-list">
+            <ol className={styles["recipe-details-content__instructions-list"]}>
               {recipe.instructions.map((instruction, index) => (
                 <li
                   key={index}
-                  className="recipe-details-content__instructions-list--option"
+                  className={styles["recipe-details-content__instructions-list--option"]}
                 >
                   {instruction}
                 </li>

7f92a145d0950cb2d599ce74ce322ff350cf7551
Halyna Riabokonenko <sgallina89@gmail.com>
2024-03-12 11:22:12 +0100

    add UpButton component with styles

---
+++ src/components/share_atomic/UpButton/UppButton.tsx
@@ -0,0 +1,53 @@
+import React, { useContext, useState } from "react";
+import styles from "./UppButton.module.css"
+import classNames from "classnames";
+import { ModeContext } from "../../../providers/mode";
+
+export const UpButton = () => {
+    const { mode } = useContext(ModeContext);
+    const [isVisible, setIsVisible] = useState(false);
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    };
+
+    const handleScroll = () => {
+        if (window.scrollY > 300) {
+            setIsVisible(true);
+        } else {
+            setIsVisible(false);
+        }
+    };
+
+    React.useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    return (
+        <div className={classNames(
+            styles["up-button-container"],
+            { [styles['show']]: isVisible, [styles['hide']]: !isVisible }
+        )}>
+            <button onClick={scrollToTop} className={classNames(
+                styles["up-button"],
+                styles[mode]
+            )}>
+                {mode === "light" ? (
+                    <img src="/images/up-button/up-light.png" alt="Up icon" className={classNames(
+                        styles["up-button__img"],
+                        styles[mode])} />
+                ) : (
+                    <img src="/images/up-button/up-dark.png" alt="Up icon" className={classNames(
+                        styles["up-button__img"],
+                        styles[mode]
+                    )} />
+                )}</button>
+        </div >
+    )
+}

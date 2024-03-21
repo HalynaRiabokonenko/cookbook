69eff48695157c86ca879fbe21df4143016c2620
Halyna Riabokonenko <sgallina89@gmail.com>
2024-03-21 22:08:57 +0100

    change RecipesFullContent by adding  clean up the listeners when component unmouns

--- src/components/share_structures/Recipes/RecipesFullContent/RecipesFullContent.tsx
+++ src/components/share_structures/Recipes/RecipesFullContent/RecipesFullContent.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { DocumentData, collection, onSnapshot } from "firebase/firestore";
+import React, { useEffect, useState, useRef } from "react";
+import { DocumentData, collection, onSnapshot, Unsubscribe } from "firebase/firestore";
 import RecipeInterface from "../../../pages/Recipes/Recipes.types";
 import { db } from "../../../../api/firebaseConfig";
 import { RecipeOption } from "../RecipeOption/RecipeOption";
@@ -8,29 +8,34 @@ const recipes: Array<string> = ["american", "georgian", "german", "indian", "ita
 
 export const RecipesFullContent = () => {
     const [recipesData, setRecipesData] = useState<RecipeInterface[]>([]);
+    const unsubscribeRefs = useRef<Unsubscribe[]>([]);
 
     const getData = (): void => {
         recipes.forEach((option) => {
             const recipesCollection = collection(db, `${option}-recipes`);
-            onSnapshot(recipesCollection, (res: { docs: DocumentData[] }) => {
+            const unsubscribe = onSnapshot(recipesCollection, (res: { docs: DocumentData[] }) => {
                 const fetchedRecipes: RecipeInterface[] = res.docs.map(doc => ({
                     id: doc.id,
                     ...doc.data()
                 }));
                 setRecipesData(prevState => [...prevState, ...fetchedRecipes]);
             });
+            unsubscribeRefs.current.push(unsubscribe);
         });
     };
 
     useEffect(() => {
         getData();
+        return () => {
+            unsubscribeRefs.current.forEach(unsubscribe => unsubscribe());
+        };
     }, []);
 
 
     return (
         <>
-            {recipesData.map((recipe) => {
-                return <div key={recipe.id}><RecipeOption recipe={recipe} /></div>;
+            {recipesData.map((recipe, id) => {
+                return <div key={`${id}-${recipe.id}`}><RecipeOption recipe={recipe} /></div>;
             })}
         </>
     );

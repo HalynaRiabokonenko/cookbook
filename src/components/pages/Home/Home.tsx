f955851515ad85aefccd741b5540e8cd38c5867b
Halyna Riabokonenko <sgallina89@gmail.com>
2024-03-22 14:38:28 +0100

    change home page by getting cuisines data from firebase

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useContext } from "react";
-import RecipesDataInterface from "./Home.types";
 import { Link } from "react-router-dom";
 import styles from "./Home.module.css";
 import { ModeContext } from "../../../providers/mode";
@@ -8,22 +7,43 @@ import Button from "../../share_atomic/Button/Button";
 import PageHeader from "../../share_atomic/PageHeader/PageHeader";
 import { User } from "firebase/auth";
 import { Page } from "../../share_structures/Page/Page";
+import { DocumentData, collection, onSnapshot } from "firebase/firestore";
+import { db } from "../../../api/firebaseConfig";
 interface HomeProps {
     user: User | null;
 }
 
+interface CuisinesInterface {
+    id: string;
+    description: string
+}
+
 const Home = ({ user }: HomeProps) => {
     const { mode } = useContext(ModeContext);
-    const [recipesData, setRecipesData] = useState<RecipesDataInterface | null>(null);
+    const [cuisinesData, setCuisinesData] = useState<CuisinesInterface[]>([]);
+
 
     useEffect(() => {
-        fetch("/recipes/recipes-ukrainian.json")
-            .then((res) => res.json())
-            .then((data) =>
-                setRecipesData(data)
-            );
-    }, []);
+        let unsubscribe: () => void;
 
+        const fetchData = async () => {
+            try {
+                const recipesCollection = collection(db, "cuisines");
+                unsubscribe = onSnapshot(recipesCollection, (snapshot: { docs: DocumentData[] }) => {
+                    const fetchedRecipes: CuisinesInterface[] = snapshot.docs.map(doc => ({
+                        id: doc.id,
+                        ...doc.data()
+                    }));
+                    setCuisinesData(fetchedRecipes);
+                });
+            } catch (error) {
+                console.error("Error fetching cuisines:", error);
+            }
+        };
+
+        fetchData();
+
+    }, []);
 
     return (
         <Page>
@@ -50,72 +70,6 @@ const Home = ({ user }: HomeProps) => {
                 “A recipe has no soul. You, as the cook, must bring soul to the
                 recipe.” – Thomas Keller
             </p>
-            {recipesData && <div className={styles["container-home__list--popular"]}>
-                <div>
-                    <ul className={styles["home__list--popular"]}>
-                        {recipesData.recipes
-                            .filter((recipe) => [1, 3, 6].includes(recipe.id))
-                            .map((recipe) => (
-                                <li
-                                    key={recipe.id}
-                                    className={classnames(
-                                        styles["home-content__recipes-list--option"],
-                                        styles[mode]
-                                    )}
-                                >
-                                    <h2 className={styles["home-content__recipes-name"]}>
-                                        {recipe.name}
-                                    </h2>
-                                    <div className={styles["home-content__recipes-info"]}>
-                                        <p className={styles["home-content__recipes-description"]}>
-                                            {recipe.description}
-                                        </p>
-                                        <div className={styles["home-content__recipes-content"]}>
-                                            <div className={styles["home-content__recipes-photo-container"]}>
-                                                <img
-                                                    src={recipe.photoPath}
-                                                    alt={recipe.name}
-                                                    className={styles["home-content__recipes-photo"]}
-                                                ></img>
-                                            </div>
-                                            <div className={styles["home-content__recipes-ingredients-container"]}>
-                                                <h3 className={styles["home-content__recipes-ingredients"]}>
-                                                    ingredients:
-                                                </h3>
-                                                <ul className={styles["home-content__ingredients-list"]}>
-                                                    {recipe.ingredients.map((ingredient, index) => (
-                                                        <li
-                                                            key={index}
-                                                            className={styles["home-content__ingredients-list--option"]}
-                                                        >
-                                                            {ingredient}
-                                                        </li>
-                                                    ))}
-                                                </ul>
-                                            </div>
-                                            <div className={styles["home-content__recipes-instructions-container"]}>
-                                                <h3 className={styles["home-content__recipes-instructions"]}>
-                                                    instructions:
-                                                </h3>
-                                                <ol className={styles["home-content__instructions-list"]}>
-                                                    {recipe.instructions.map((instruction, index) => (
-                                                        <li
-                                                            key={index}
-                                                            className={styles["home-content__instructions-list--option"]}
-                                                        >
-                                                            {instruction}
-                                                        </li>
-                                                    ))}
-                                                </ol>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </li>
-                            ))}
-                    </ul>
-                </div>
-            </div>}
-
             {user && <div className={styles["home-content__all-recipes-link-container"]}>
                 <Link to="/recipes" >
                     <Button mode={mode}>
@@ -134,6 +88,21 @@ const Home = ({ user }: HomeProps) => {
                         </Link>
                     </div>
                 </div>}
+            <div className={styles["container-home__list--popular"]}>
+                <ul className={styles["home__list--popular"]}>
+                    {cuisinesData.map((cousine) => {
+                        return (
+                            <li key={cousine.id} className={classnames(
+                                styles["home-content__recipes-list--option"],
+                                styles[mode]
+                            )}>
+                                <h2 className={styles["home-content__recipes-name"]}>{cousine.id}</h2>
+                                <p className={styles["home-content__recipes-description"]}>{cousine.description}</p>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
         </Page>
 
     );

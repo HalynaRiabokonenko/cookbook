b7c68deab51df11ff37e82a895232534f363cf22
Halyna Riabokonenko <sgallina89@gmail.com>
2024-03-29 18:54:14 +0100

    content and styles About page

--- src/components/pages/About/About.tsx
+++ src/components/pages/About/About.tsx
@@ -13,57 +13,62 @@ const About = () => {
             <PageHeader mode={mode}>
                 About us
             </PageHeader>
-            <p className={classnames(
-                styles["about-content__paragraph"],
-                styles["about-content__paragraph--italic"]
-            )}>
-                Welcome to PROVEN RECIPES – Your Culinary Journey to Ukraine!
-            </p>
-            <p className={styles["about-content__paragraph"]}>
-                At PROVEN RECIPES, we are passionate about sharing the rich and
-                diverse flavors of Ukrainian cuisine with the world. Our mission is to
-                bring the authentic taste of Ukraine to your kitchen, one recipe at a
-                time.
-            </p>
-            <h2 className={styles["about-content__subheader"]}>Our Story</h2>
-            <p className={styles["about-content__paragraph"]}>
-                Founded with a love for traditional Ukrainian cooking, PROVEN RECIPES
-                is a labor of love that stems from the deep-rooted culinary traditions
-                of Ukraine. Our team of dedicated food enthusiasts and chefs curate a
-                collection of mouthwatering recipes that reflect the true essence of
-                Ukrainian gastronomy.
-            </p>
-            <h2 className={styles["about-content__subheader"]}>What Sets Us Apart</h2>
-            <p className={styles["about-content__paragraph"]}>
-                What sets us apart is our commitment to authenticity. Each recipe
-                featured on our website is carefully researched, tested, and presented
-                with detailed instructions, ensuring that you can recreate the flavors
-                of Ukraine in your home kitchen. We believe in the power of good food
-                to connect people, and we strive to create a community where food
-                lovers and cooking enthusiasts can come together to explore the
-                delicious world of Ukrainian cuisine.
-            </p>
-            <h2 className={styles["about-content__subheader"]}>
-                Join Us on This Culinary Adventure
-            </h2>
-            <p className={styles["about-content__paragraph"]}>
-                Whether you are a seasoned chef or a novice in the kitchen, PROVEN
-                RECIPES welcomes you to embark on this culinary adventure with us.
-                Discover the secrets of borscht, pierogi, and other Ukrainian
-                classics, and explore regional specialties that showcase the diverse
-                culinary landscape of Ukraine.
-            </p>
-            <p className={styles["about-content__paragraph"]}>
-                Thank you for being a part of our community. We invite you to explore
-                our recipes, try your hand at cooking authentic Ukrainian dishes, and
-                savor the delightful flavors that Ukraine has to offer.
-            </p>
-            <p className={classnames(
-                styles["about-content__paragraph"],
-                styles["about-content__paragraph--italic"]
-            )}>
-                Дякуємо (Thank you) for visiting PROVEN RECIPES!
-            </p>
+            <div className={styles["about-content"]}>
+                <div className={classnames(
+                    styles["about-content__paragraph-container"],
+                    styles[mode]
+                )}>
+                    <p className={classnames(
+                        styles["about-content__paragraph"],
+                        styles[mode]
+                    )}>Welcome to Proven Recipes, your culinary journey across the globe!</p>
+                </div>
+                <div className={classnames(
+                    styles["about-content__paragraph-container"],
+                    styles[mode]
+                )}>
+                    <p className={classnames(
+                        styles["about-content__paragraph"],
+                        styles[mode]
+                    )}>At Proven Recipes, we're enthusiastic about sharing the rich and diverse flavors of cuisines from around the world. Our mission is to bring the authentic tastes of different cultures to your kitchen, one recipe at a time.</p>
+                </div>
+                <div className={classnames(
+                    styles["about-content__paragraph-container"],
+                    styles[mode]
+                )}>
+                    <p className={classnames(
+                        styles["about-content__paragraph"],
+                        styles[mode]
+                    )}>Founded with a love for traditional culinary arts, Proven Recipes is a project born out of the deep-rooted culinary traditions of various countries. Our team of dedicated enthusiasts and chefs curates a collection of delicious recipes that reflect the true essence of global gastronomy.</p>
+                </div>
+                <div className={classnames(
+                    styles["about-content__paragraph-container"],
+                    styles[mode]
+                )}>
+                    <p className={classnames(
+                        styles["about-content__paragraph"],
+                        styles[mode]
+                    )}>What sets us apart is our commitment to authenticity. Every recipe featured on our website is meticulously researched, tested, and presented with detailed instructions, allowing you to recreate global flavors in your home kitchen. We believe in the power of good food to bring people together, and we strive to create a community where food lovers and culinary enthusiasts can come together to explore the delicious world of international cuisine.</p>
+                </div>
+                <div className={classnames(
+                    styles["about-content__paragraph-container"],
+                    styles[mode]
+                )}>
+                    <p className={classnames(
+                        styles["about-content__paragraph"],
+                        styles[mode]
+                    )}>Whether you're a seasoned chef or a kitchen novice, Proven Recipes invites you to embark on this culinary journey with us. Discover the secrets of classics like borscht, pierogi, and other dishes from around the world, and explore regional specialties that showcase the diverse culinary landscape of different countries.</p>
+                </div>
+                <div className={classnames(
+                    styles["about-content__paragraph-container"],
+                    styles[mode]
+                )}>
+                    <p className={classnames(
+                        styles["about-content__paragraph"],
+                        styles[mode]
+                    )}>Thank you for being a part of our community. We invite you to explore our recipes, try your hand at cooking authentic dishes from various countries, and savor the delightful flavors that the world has to offer.</p>
+                </div>
+            </div>
         </Page>
     );
 }

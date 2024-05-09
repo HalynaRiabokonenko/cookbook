d1f7891b929a930082947312b921c4eeee0cd507
Halyna Riabokonenko <sgallina89@gmail.com>
2024-05-09 11:22:02 +0200

    add change account details functionality

--- src/components/pages/Account/Account.tsx
+++ src/components/pages/Account/Account.tsx
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useModeContext } from "../../../providers/mode";
 import classnames from "classnames";
 import styles from "./Account.module.css";
 import PageHeader from "../../share_atomic/PageHeader/PageHeader";
 import { Page } from "../../share_structures/Page/Page";
-import { User } from "firebase/auth";
+import { User, getIdTokenResult } from "firebase/auth";
 import Button from "../../share_atomic/Button/Button";
 import { useNavigate } from "react-router-dom";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { db } from "../../../api/firebaseConfig";
 
 interface AccountProps {
     user: User | null;
 }
 
+interface UserData {
+    name: string;
+    surname: string;
+    username: string;
+    male: boolean;
+    birthDate: any;
+}
+
 export const Account = ({ user }: AccountProps) => {
     const { mode } = useModeContext();
     const navigate = useNavigate();
+    const [readonly, setReadOnly] = useState<boolean>(true)
+    const [userData, setUserData] = useState<UserData | null>(null);
+    const [formData, setFormData] = useState<UserData>({
+        name: "",
+        surname: "",
+        username: "",
+        male: true,
+        birthDate: ""
+    });
 
     let creationTime = "";
     let lastSignInTime = "";
@@ -23,6 +42,69 @@ export const Account = ({ user }: AccountProps) => {
         creationTime = new Date(user.metadata.creationTime).toLocaleString();
         lastSignInTime = new Date(user.metadata.lastSignInTime).toLocaleString();
     }
+
+    useEffect(() => {
+        if (!user) {
+            console.error("User is empty");
+            return;
+        }
+
+        const fetchUserData = async () => {
+            try {
+                const docRef = doc(db, `userData/${user.uid}`);
+                const docSnap = await getDoc(docRef);
+                if (docSnap.exists()) {
+                    setUserData(docSnap.data() as UserData);
+                    setFormData(docSnap.data() as UserData)
+                }
+
+            } catch (error) {
+                console.error("Error fetching user data:", error);
+            }
+        };
+        fetchUserData();
+    }, [user]);
+
+    const handleSaveData = async (data: UserData) => {
+        try {
+            await setDoc(doc(db, `userData/${user?.uid}`), data);
+            setUserData(data);
+            setReadOnly(true)
+        } catch (error) {
+            console.error("Error saving user data:", error);
+        }
+    };
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        handleSaveData(formData);
+    };
+
+    const handleReset = () => {
+        setReadOnly(true);
+        if (userData) {
+            setFormData(userData);
+        } else {
+            setFormData(
+                {
+                    name: "",
+                    surname: "",
+                    username: "",
+                    male: true,
+                    birthDate: ""
+                }
+            )
+        }
+    };
+
 
     return (
         <Page>
@@ -33,19 +115,134 @@ export const Account = ({ user }: AccountProps) => {
                 styles["account__content"],
                 styles[mode]
             )}>
-                <div className={classnames(
-                    styles["account__user-info-container"],
-                    styles[mode]
-                )}>
-                    Hello, <div className={classnames(
-                        styles["account__user-info"],
-                        styles[mode]
-                    )}>{user?.email}</div>
-                </div>
+
                 <div className={classnames(
                     styles["account__user-details"],
                     styles[mode]
                 )}>
+                    {readonly && <div className={classnames(styles["account__button-container--edit"], styles[mode])}>
+                        <button onClick={() => { setReadOnly(false) }} className={classnames(styles["account__button--edit"], styles[mode])}>
+                            {mode === "light" ? (
+                                <img src="/images/account/edit-light.png" className={styles["account__button--edit-icon"]} />
+
+                            ) : (
+                                <img src="/images/account/edit-white.png" className={styles["account__button--edit-icon"]} />
+                            )}
+                        </button>
+                    </div>}
+                    {user && <form onSubmit={handleSubmit}>
+                        <div className={classnames(
+                            styles["account__form--container"],
+                            styles[mode]
+                        )}>
+                            <div className={classnames(
+                                styles["account__form--inputs-container"],
+                                styles[mode]
+                            )}>
+                                <label htmlFor="name" className={classnames(
+                                    styles["account__form--label"],
+                                    styles[mode]
+                                )}>
+                                    Email:
+                                </label>
+                                <div className={classnames(
+                                    styles["account__form--input-text"],
+                                    styles[mode]
+                                )}>{user.email}</div>
+                            </div>
+                            <div className={classnames(
+                                styles["account__form--inputs-container"],
+                                styles[mode]
+                            )}>
+                                <label htmlFor="username" className={classnames(
+                                    styles["account__form--label"],
+                                    styles[mode]
+                                )}>
+                                    Username:
+                                </label>
+                                <input type="text" name="username" value={formData.username} onChange={handleChange} readOnly={readonly}
+                                    className={readonly ?
+                                        classnames(
+                                            styles["account__form--input-readonly"],
+                                            styles[mode]) :
+                                        classnames(
+                                            styles["account__form--input"],
+                                            styles[mode])
+                                    }
+                                />
+                            </div>
+                            <div className={classnames(
+                                styles["account__form--inputs-container"],
+                                styles[mode]
+                            )}>
+                                <label htmlFor="name" className={classnames(
+                                    styles["account__form--label"],
+                                    styles[mode]
+                                )}>
+                                    Name:
+                                </label>
+                                <input type="text" name="name" value={formData.name} onChange={handleChange} readOnly={readonly}
+                                    className={readonly ?
+                                        classnames(
+                                            styles["account__form--input-readonly"],
+                                            styles[mode]) :
+                                        classnames(
+                                            styles["account__form--input"],
+                                            styles[mode])
+                                    }
+                                />
+                            </div>
+                            <div className={classnames(
+                                styles["account__form--inputs-container"],
+                                styles[mode]
+                            )}>
+                                <label htmlFor="surname" className={classnames(
+                                    styles["account__form--label"],
+                                    styles[mode]
+                                )}>
+                                    Surname:
+                                </label>
+                                <input type="text" name="surname" value={formData.surname} onChange={handleChange} readOnly={readonly}
+                                    className={readonly ?
+                                        classnames(
+                                            styles["account__form--input-readonly"],
+                                            styles[mode]) :
+                                        classnames(
+                                            styles["account__form--input"],
+                                            styles[mode])
+                                    }
+                                />
+                            </div>
+                            <div className={classnames(
+                                styles["account__form--inputs-container"],
+                                styles[mode]
+                            )}>
+                                <label htmlFor="birthDate" className={classnames(
+                                    styles["account__form--label"],
+                                    styles[mode]
+                                )}>
+                                    Birth Date:
+                                </label>
+                                <input type="date" name="birthDate" value={formData.birthDate} onChange={handleChange} readOnly={readonly}
+                                    className={readonly ?
+                                        classnames(
+                                            styles["account__form--input-readonly"],
+                                            styles[mode]) :
+                                        classnames(
+                                            styles["account__form--input"],
+                                            styles[mode])
+                                    }
+                                />
+                            </div>
+
+
+                        </div>
+                        {!readonly && <div className={styles["account__buttons--container"]}>
+                            <Button type="reset" onClick={handleReset}>Cancel</Button>
+                            <Button type="submit">Submit</Button>
+                        </div>
+                        }
+                    </form>}
                     <div className={classnames(
                         styles["account__user-detail-container"],
                         styles[mode]
@@ -86,8 +283,13 @@ export const Account = ({ user }: AccountProps) => {
                         )}> {lastSignInTime}</p>
                     </div>
                 </div>
-                <Button onClick={() => { navigate("/change-password") }}>Change Password</Button>
-                <Button onClick={() => { navigate("/delete-account") }}>Delete account</Button>
+                {
+                    readonly && <div>
+                        <Button onClick={() => { navigate("/change-password") }}>Change Password</Button>
+                        <Button onClick={() => { navigate("/delete-account") }}>Delete account</Button>
+                    </div>
+                }
+
             </div>
         </Page>
     )

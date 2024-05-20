7fdc998b8263f55e5f58391b9ad68c983a43579b
Halyna Riabokonenko <sgallina89@gmail.com>
2024-05-20 18:13:51 +0200

    change tailwind styles and text for NotFound

--- src/components/pages/NotFound/NotFound.tsx
+++ src/components/pages/NotFound/NotFound.tsx
@@ -1,33 +1,36 @@
 import React from "react";
-import { useModeContext } from "../../../providers/mode";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import styles from "./NotFound.module.css";
 import classnames from "classnames";
-import PageHeader from "../../atomic/PageHeader/PageHeader";
 import { Page } from "../../structures/Page/Page";
 import { Button } from '@radix-ui/themes'
-import { AlertDialog, AlertDialogTrigger, AlertDialogContent, AlertDialogTitle, AlertDialogDescription, AlertDialogAction } from '@radix-ui/react-alert-dialog';
 
 const NotFound = () => {
     const navigate = useNavigate();
 
     return (
         <Page>
-            <div className="flex items-center justify-center min-h-screen">
-                <div className="text-center">
-                    <h1 className={classnames(
-                        "text-9xl font-bold text-redMedium",
-                        styles["animate-fade-in"])}>404</h1>
-                    <p className="mt-4 text-lg text-gray-600">Page Not Found</p>
-
-                    <Button onClick={() => { navigate("/") }} className="mt-6 px-4 py-2 text-white bg-blue-600 rounded hover:bg-blue-700 focus:outline-none">
-                        Go Home
-                    </Button>
+            <div className="flex items-center justify-center content-center min-h-screen -mt-9">
+                <div className="text-center flex">
+                    <div>
+                        <h1 className={classnames(
+                            "text-[14rem] font-bold text-redMedium",
+                            styles["animate-fade-in"])}>404</h1>
+                        <p className="mt-4 text-lg text-gray-600">
+                            Oops! The page you are looking for does not exist.
+                        </p>
+                        <p className="mt-2 text-gray-600">
+                            It might have been moved or deleted. Please check the URL and try again.
+                        </p>
+                        <Button onClick={() => { navigate("/") }} className="mt-6 px-4 py-2 text-white bg-blue-600 rounded hover:bg-blue-700 focus:outline-none">
+                            Home
+                        </Button>
+                    </div>
+                    <div></div>
                 </div>
             </div>
         </Page>
     )
 }
-
 export default NotFound;
 

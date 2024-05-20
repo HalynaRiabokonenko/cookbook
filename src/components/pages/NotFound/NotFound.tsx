8fb9eb4505ca90068c23b43c85312b3cce9f237b
Halyna Riabokonenko <sgallina89@gmail.com>
2024-05-20 18:18:18 +0200

    add dark mode text colot to NotFound

--- src/components/pages/NotFound/NotFound.tsx
+++ src/components/pages/NotFound/NotFound.tsx
@@ -4,8 +4,10 @@ import styles from "./NotFound.module.css";
 import classnames from "classnames";
 import { Page } from "../../structures/Page/Page";
 import { Button } from '@radix-ui/themes'
+import { useModeContext } from "../../../providers/mode";
 
 const NotFound = () => {
+    const { mode } = useModeContext();
     const navigate = useNavigate();
 
     return (
@@ -16,10 +18,10 @@ const NotFound = () => {
                         <h1 className={classnames(
                             "text-[14rem] font-bold text-redMedium",
                             styles["animate-fade-in"])}>404</h1>
-                        <p className="mt-4 text-lg text-gray-600">
+                        <p className={classnames("mt-4 text-lg", mode === "dark" ? "text-gray-300" : "text-gray-600")}>
                             Oops! The page you are looking for does not exist.
                         </p>
-                        <p className="mt-2 text-gray-600">
+                        <p className={classnames("mt-2", mode === "dark" ? "text-gray-300" : "text-gray-600")}>
                             It might have been moved or deleted. Please check the URL and try again.
                         </p>
                         <Button onClick={() => { navigate("/") }} className="mt-6 px-4 py-2 text-white bg-blue-600 rounded hover:bg-blue-700 focus:outline-none">

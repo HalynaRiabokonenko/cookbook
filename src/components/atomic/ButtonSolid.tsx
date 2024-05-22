9c5f1b3f92747242aba424b8a6e459b2ddfd9bdf
Halyna Riabokonenko <sgallina89@gmail.com>
2024-05-22 14:20:04 +0200

    ButtonSolid, ButtonOutline at Account

--- src/components/atomic/ButtonSolid.tsx
+++ src/components/atomic/ButtonSolid.tsx
@@ -16,9 +16,9 @@ export const ButtonSolid = ({ children, onClick }: ButtonSolidProps) => {
             variant="solid"
             size="3"
             className={mode === 'dark' ?
-                "mt-6 px-4 py-2  rounded-md bg-mediumGreen text-mediumGreenDark  text-l font-semibold leading-6 shadow-sm border border-solid border-1.5 border-mediumGreen  hover:bg-inherit hover:text-mediumGreen focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                "mx-4 mt-6 px-9 py-2 rounded-md bg-mediumGreen text-mediumGreenDark  text-l font-semibold leading-6 shadow-sm border border-solid border-1.5 border-mediumGreen  hover:bg-inherit hover:text-mediumGreen focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 :
-                "mt-6 px-4 py-2  rounded-md bg-mediumGreen text-lightGreen  text-l font-semibold leading-6 shadow-sm border border-solid border-1.5 border-mediumGreen  hover:bg-inherit hover:text-mediumGreen focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                "mx-4 mt-6 px-9 py-2 rounded-md bg-mediumGreen text-lightGreen  text-l font-semibold leading-6 shadow-sm border border-solid border-1.5 border-mediumGreen  hover:bg-inherit hover:text-mediumGreen focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             }
         >{children}</Button>
     )

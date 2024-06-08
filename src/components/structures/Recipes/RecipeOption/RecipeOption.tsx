7a47f6c3b604c9e21b21341894eb7c835aae7009
Halyna Riabokonenko <sgallina89@gmail.com>
2024-06-08 17:09:27 +0200

    add HoverCard to RecipeOption

--- src/components/structures/Recipes/RecipeOption/RecipeOption.tsx
+++ src/components/structures/Recipes/RecipeOption/RecipeOption.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import classnames from "classnames";
 import { useModeContext } from "../../../../providers/mode";
 import styles from "./RecipeOption.module.css";
 import * as AspectRatio from '@radix-ui/react-aspect-ratio';
 import { Recipe } from "../../../../commons/types/Recipe";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from '@radix-ui/react-hover-card';
+import { Avatar, Box, Text } from "@radix-ui/themes";
+import { DocumentData, collection, onSnapshot } from "firebase/firestore";
+import { Cuisine } from "../../../../commons/types/Cuisine";
+import { db } from "../../../../api/firebaseConfig";
 
 interface RecipeOptionTypes {
     recipe: Recipe;
@@ -12,10 +17,36 @@ interface RecipeOptionTypes {
 
 export const RecipeOption = ({ recipe }: RecipeOptionTypes) => {
     const { mode } = useModeContext();
+    const [cuisinesData, setCuisinesData] = useState<Cuisine[]>([]);
+
+    useEffect(() => {
+        const unsubscribeCuisines = onSnapshot(collection(db, "cuisines"), (snapshot) => {
+            const fetchedCuisines: Cuisine[] = snapshot.docs.map((doc: DocumentData) => {
+                const data = doc.data();
+                return {
+                    id: doc.id,
+                    img: data.img || '', // Убедитесь, что есть значение по умолчанию
+                    description: data.description || '', // Убедитесь, что есть значение по умолчанию
+                };
+            });
+            setCuisinesData(fetchedCuisines);
+        });
+
+        return () => {
+            unsubscribeCuisines();
+        };
+    }, []);
+
+    console.log("CUISINES DATA:", cuisinesData);
+
+    let cuisineSrc = (cuisineOption: string) => {
+        const cuisine = cuisinesData.find(el => el.id === cuisineOption);
+        return cuisine ? cuisine.img : '';
+    };
 
     return (
         <div className="recipe-option__container">
-            <Link to={`/recipes/${recipe.option}/${recipe.id}`} key={recipe.id} className={classnames(
+            <Link to={`/recipes/${recipe.option}/${recipe.id}`} className={classnames(
                 styles["recipes-content__recipes-link"],
                 styles[mode]
             )}>
@@ -32,14 +63,35 @@ export const RecipeOption = ({ recipe }: RecipeOptionTypes) => {
                             />
                         </AspectRatio.Root>
                     </div>
-                    <div className={styles["recipes-content__recipes-container"]}>
-                        <h2 className={styles["recipes-content__recipes-name"]}>
-                            {recipe.name}
-                        </h2>
+                    <div className="flex justify-center items-center h-[10%] z-[999] relative">
+                        <HoverCard>
+                            <HoverCardTrigger asChild>
+                                <div className={`relative capitalize flex flex-row items-center justify-center h-full`}>
+                                    {recipe.name}
+                                </div>
+                            </HoverCardTrigger>
+                            <HoverCardContent className={`max-w-[300px] p-4 shadow-lg rounded-lg ${mode === 'dark' ? 'bg-midnightMoss' : 'bg-white'}`}>
+                                <div className="flex items-start space-x-4 h-full">
+                                    <div className="h-full w-full flex flex-col items-start ml-3">
+                                        <Avatar
+                                            size="3"
+                                            fallback="R"
+                                            radius="full"
+                                            src={cuisineSrc(recipe.option)}
+                                            alt={recipe.name}
+                                        />
+                                    </div>
+                                    <Box className="flex flex-col items-start h-full">
+                                        <Text as="div" size="1" className="font-light text-sm text-justify mr-3">
+                                            {recipe.description}
+                                        </Text>
+                                    </Box>
+                                </div>
+                            </HoverCardContent>
+                        </HoverCard>
                     </div>
                 </div>
             </Link>
         </div>
-
     );
 };

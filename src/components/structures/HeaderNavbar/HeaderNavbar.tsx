e802284062b968da1895c3fb7b5be6e6105a5750
Halyna Riabokonenko <sgallina89@gmail.com>
2024-06-15 23:28:19 +0200

    HeaderNavbar NavigationMenu radix-ui

--- src/components/structures/HeaderNavbar/HeaderNavbar.tsx
+++ src/components/structures/HeaderNavbar/HeaderNavbar.tsx
@@ -1,58 +1,94 @@
-import React from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import styles from "./HeaderNavbar.module.css";
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import classnames from "classnames";
 import { useModeContext } from "../../../providers/mode";
 import { User } from "firebase/auth";
 import { Button } from "@radix-ui/themes";
-import { HomeIcon } from "@radix-ui/react-icons";
+import * as NavigationMenu from '@radix-ui/react-navigation-menu';
+import { DocumentData, collection, onSnapshot } from "firebase/firestore";
+import { db } from "../../../api/firebaseConfig";
+import { Cuisine } from "../../../commons/types/Cuisine";
+import styles from "./HeaderNavbar.module.css"
 
 interface HeaderNavbarProps {
     user: User | null;
 }
 
+
 export const HeaderNavbar = ({ user }: HeaderNavbarProps) => {
     const { mode } = useModeContext();
     const location = useLocation();
     const navigate = useNavigate();
+    const [cuisinesData, setCuisinesData] = useState<Cuisine[]>([]);
+
+    useEffect(() => {
+        const unsubscribeCuisines = onSnapshot(collection(db, "cuisines"), (snapshot: { docs: DocumentData[] }) => {
+            const fetchedCuisines: Cuisine[] = snapshot.docs.map(doc => ({
+                id: doc.id,
+                ...doc.data()
+            }));
+            setCuisinesData(fetchedCuisines);
+        });
+
+        return () => {
+            unsubscribeCuisines();
+        };
+    }, []);
 
     return (
-        <div className={styles["global-nav__container"]}>
-            <nav className={styles["global-nav"]}>
-                <ul className={styles["global-nav__list"]}>
-                    <li>
-                        <Link
-                            className={classnames(
-                                styles["global-nav__list-item-link"],
-                                styles[mode]
+        <NavigationMenu.Root className="relative z-[3] flex justify-end">
+            <NavigationMenu.List className="m-0 flex items-center justify-center list-none rounded-[6px] p-1">
+                <NavigationMenu.Item className={location.pathname === '/recipes' || location.pathname.includes('/recipes/') ? styles.active : ''}>
+                    <NavigationMenu.Trigger className="h-headerHeight group flex select-none items-center rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none outline-none" >
+                        <Button
+                            className={classnames("uppercase border-none bg-none cursor-pointer flex justify-center items-center rounded py-2.5 px-7 rounded-xl bg-inherit leading-6 font-semibold",
+                                {
+                                    "hover:bg-optionHoverDark":
+                                        mode === 'dark',
+                                    "hover:bg-optionHover":
+                                        mode !== 'dark',
+                                }
                             )}
-                            to="/recipes"
+                            onClick={() => {
+                                navigate("/recipes");
+                            }}
                         >
-                            <div
-                                className={classnames(
-                                    styles["global-nav__list-item"],
-                                    styles[mode],
-                                    {
-                                        [styles.active]: location.pathname === '/recipes' || location.pathname.includes('/recipes/')
-                                    }
-                                )}
-                            >
-                                <div className={classnames(
-                                    "uppercase rounded px-9 py-2.5 rounded-xl bg-inherit text-l font-semibold leading-6",
-                                    {
-                                        "hover:bg-optionHoverDark":
-                                            mode === 'dark',
-                                        "hover:bg-optionHover":
-                                            mode !== 'dark',
-                                    }
-                                )}>
-                                    Recipes
-                                </div>
-                            </div>
-                        </Link>
-                    </li>
-                    {!user && (
-                        <li className="flex items-center h-full">
+                            Recipes
+                        </Button>
+                    </NavigationMenu.Trigger>
+                    <NavigationMenu.Content
+                        className={classnames("absolute z-[3] top-[70px] left-0 w-full sm:w-auto rounded-md",
+                            {
+                                "bg-mediumGreenDark":
+                                    mode === 'dark',
+                                "bg-white shadow-xl":
+                                    mode !== 'dark',
+                            })
+                        }
+                    >
+                        <ul className="m-0  px-9 list-none w-full my-4">
+                            {cuisinesData.map((cuisine) => (
+                                <li
+                                    key={cuisine.id}
+                                    className={classnames("capitalize text-lg rounded-md font-normal h-full w-full flex justify-center items-center cursor-pointer py-2 px-9",
+                                        {
+                                            "hover:bg-optionHoverDark":
+                                                mode === 'dark',
+                                            "hover:bg-optionHover":
+                                                mode !== 'dark',
+                                        }
+                                    )}
+                                    onClick={() => navigate(`/recipes/${cuisine.id}`)}
+                                >
+                                    {cuisine.id}
+                                </li>
+                            ))}
+                        </ul>
+                    </NavigationMenu.Content>
+                </NavigationMenu.Item>
+                {!user && (
+                    <NavigationMenu.Item>
+                        <NavigationMenu.Trigger>
                             <Button
                                 className={classnames(
                                     "uppercase mx-4 rounded px-9 py-2 rounded-xl bg-inherit text-l font-semibold leading-6 shadow-sm border border-solid",
@@ -69,10 +105,12 @@ export const HeaderNavbar = ({ user }: HeaderNavbarProps) => {
                             >
                                 Login
                             </Button>
-                        </li>
-                    )}
-                </ul>
-            </nav>
-        </div>
+                        </NavigationMenu.Trigger>
+                    </NavigationMenu.Item>
+                )}
+            </NavigationMenu.List>
+
+        </NavigationMenu.Root >
     );
+
 };

ce62dd6192e35fbef1d92a78cc080fa01d46ed12
Halyna Riabokonenko <sgallina89@gmail.com>
2024-06-30 12:27:53 +0200

    Favorites.tsx show favorite recipes from firebase

--- src/components/pages/Favorites/Favorites.tsx
+++ src/components/pages/Favorites/Favorites.tsx
@@ -5,14 +5,14 @@ import { Recipe } from '../../../commons/types/Recipe';
 import { Page } from '../../structures/Page/Page';
 import { PageHeader } from '../../atomic/PageHeader/PageHeader';
 import { db } from '../../../api/firebaseConfig';
-import { collection, getDocs, DocumentData } from 'firebase/firestore';
+import { collection, getDocs, DocumentData, onSnapshot, QuerySnapshot } from 'firebase/firestore';
 
 interface FavoritesProps {
     user: User | null;
 }
 
 export const Favorites = ({ user }: FavoritesProps) => {
-    const [favoritesData, setFavoritesData] = useState<Recipe[]>([]);
+    const [recipesData, setRecipesData] = useState<Recipe[]>([]);
 
     useEffect(() => {
         const fetchFavorites = async () => {
@@ -38,7 +38,40 @@ export const Favorites = ({ user }: FavoritesProps) => {
                     });
                 });
 
-                setFavoritesData(favorites);
+                const fetchData = () => {
+                    try {
+                        const recipesCollection = collection(db, `recipes`);
+                        return onSnapshot(recipesCollection, (snapshot: QuerySnapshot<DocumentData>) => {
+                            const fetchedRecipes: Recipe[] = [];
+                            snapshot.docs.forEach(doc => {
+                                const data = doc.data();
+                                favorites.forEach(option => {
+                                    if (doc.id === option.cuisine) {
+                                        Object.keys(data).forEach((key) => {
+                                            if (key === option.id) {
+                                                fetchedRecipes.push({
+                                                    id: key,
+                                                    ...data[key]
+                                                });
+                                            }
+                                        });
+                                    }
+                                });
+                            });
+                            setRecipesData(fetchedRecipes);
+                        });
+                    } catch (error) {
+                        console.error("Error fetching recipes:", error);
+                    }
+                };
+
+                const unsubscribe = fetchData();
+
+                return () => {
+                    if (unsubscribe) {
+                        unsubscribe();
+                    }
+                };
             } catch (error) {
                 console.error('Error fetching favorites:', error);
             }
@@ -47,18 +80,21 @@ export const Favorites = ({ user }: FavoritesProps) => {
         fetchFavorites();
     }, [user]);
 
-    console.log("FAVORITES", JSON.stringify(favoritesData));
-
     return (
         <Page>
             <PageHeader>Favorite Recipes</PageHeader>
-            {favoritesData.length > 0 ? (
-                favoritesData.map((recipe) => (
-                    <RecipeOption key={Math.floor(Math.random() * Date.now())} recipe={recipe} />
-                ))
-            ) : (
-                <div className='flex w-full items-center justify-center'>No favorite recipes found.</div>
-            )}
+            <div className="flex flex-col m-8">
+                <div className="flex flex-wrap justify-evenly gap-5">
+                    {recipesData.length > 0 ? (
+                        recipesData.map((recipe) => (
+                            <RecipeOption key={Math.floor(Math.random() * Date.now())} recipe={recipe} />
+                        ))
+                    ) : (
+                        <div className='flex w-full items-center justify-center'>No favorite recipes found.</div>
+                    )}
+                </div>
+            </div>
+
         </Page>
     );
 };

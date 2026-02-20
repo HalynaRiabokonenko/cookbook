61932b5b3b6bfbc73420d6dec49c3a5cb3849907
Halyna Riabokonenko <sgallina89@gmail.com>
2026-02-20 19:26:09 +0100

    change baseURL at playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://cookbook-34a7d.web.app/',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
